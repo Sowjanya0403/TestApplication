5cd5f995957d32e03c15d1a366071a7e81785386
sowjanya.dokku <sowjanya.dokku@imclsrlrplatform.onmicrosoft.com>
2021-08-27 18:42:26 +0530

    Search Movies API Implemented

--- TestApplication/Controllers/MoviesController.cs
+++ TestApplication/Controllers/MoviesController.cs
@@ -1,22 +1,41 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using TestApplication.Models;
 
 namespace TestApplication.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/movies")]
     [ApiController]
     public class MoviesController : ControllerBase
     {
-        // GET api/values
-        [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        [Route("list")]
+        [HttpPost]
+        [ProducesResponseType(typeof(List<MoviesListObj>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult> GetMovieList([FromBody] MoviesSearchObj requestObj)
         {
-            return new string[] { "value1", "value2" };
-        }
+            List<MoviesListObj> moviesList = new List<MoviesListObj>();
 
-       
+            //Fetching Data from JSON File
+            string JSON = System.IO.File.ReadAllText(Path.Combine(Directory.GetCurrentDirectory(), $"{"movies.json"}"));
+            moviesList = Newtonsoft.Json.JsonConvert.DeserializeObject<List<MoviesListObj>>(JSON);
+
+            //Filtering Basing on Selected Langugae
+            if (requestObj.SelectedLanguage.Count() > 0)
+            {
+                moviesList = moviesList.Where(c => requestObj.SelectedLanguage.Contains(c.Language)).ToList();
+            }
+
+            // Filtering Basing on Selected Location
+            if (requestObj.SelectedLocation.Count() > 0)
+            {
+                moviesList = moviesList.Where(c => requestObj.SelectedLocation.Contains(c.Location)).ToList();
+            }
+            return Ok(moviesList);
+        }
     }
 }
---
+++ TestApplication/Models/MoviesSearchObj.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace TestApplication.Models
+{
+    public class MoviesSearchObj
+    {
+        public List<string> SelectedLanguage { get; set; }
+        public List<string> SelectedLocation { get; set; }
+    }
+}
