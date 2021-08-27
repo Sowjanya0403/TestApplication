c135eb1b33b9a910118d6bc0d183cb8ad86935d1
sowjanya.dokku <sowjanya.dokku@imclsrlrplatform.onmicrosoft.com>
2021-08-27 18:48:49 +0530

    Unit Test Project Added

---
+++ APIUnitTesting/MoviesUnitTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace APIUnitTesting
+{
+    [TestClass]
+    public class MoviesUnitTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
