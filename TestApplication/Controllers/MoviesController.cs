56a303e2fa6838676e4b50d0223940aa1f959e3d
sowjanya.dokku <sowjanya.dokku@imclsrlrplatform.onmicrosoft.com>
2021-08-27 18:10:26 +0530

    Add project files.

---
+++ TestApplication/Controllers/MoviesController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestApplication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+       
+    }
+}
